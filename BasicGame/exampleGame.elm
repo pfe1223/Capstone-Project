701184612cd45468ffeb39786493bc4464a398d4
Patrick <patrick.ester@gmail.com>
2015-02-19 09:08:06 -0600

    New Version
    I accidentally saved the old version of my code to my GitHub folder.
    This version reflects the changes to my game.

--- BasicGame/exampleGame.elm
+++ BasicGame/exampleGame.elm
@@ -1,3 +1,5 @@
+-- See this document for more information on making Pong:
+-- http://elm-lang.org/blog/Pong.elm
 import Color (..)
 import Graphics.Collage (..)
 import Graphics.Element (..)
@@ -7,131 +9,152 @@ import Text
 import Time (..)
 import Window
 
--- Inputs for the game
+-- SIGNALS
 
-type alias Input = { space:Bool, player:Int, delta:Time}
-delta : Signal Time
-delta = inSeconds <~ fps 35
+main =
+  Signal.map2 view Window.dimensions gameState
+
+gameState : Signal Game
+gameState =
+  Signal.foldp update defaultGame input
+
+delta =
+  Signal.map inSeconds (fps 35)
 
 input : Signal Input
-input = sampleOn delta <| Input <~ Keyboard.space
-                                 ~ Signal.map2 .x .y Keyboard.arrows
-                                 ~ delta
+input =
+  Signal.sampleOn delta <|
+    Signal.map4 Input
+      Keyboard.space
+      (Signal.map .x Keyboard.arrows)
+      (Signal.map .y Keyboard.arrows)
+      delta
 
--- Model for the game
+
+-- MODEL
 
 (gameWidth,gameHeight) = (600,400)
 (halfWidth,halfHeight) = (300,200)
 
-type alias Player a = { a | x:Float, y:Float, vx:Float, vy:Float }
-type alias GoodGuy = Player { score:Int }
-type alias BadGuy = Player {}
 type State = Play | Pause
-type alias Game = { state:State, goodGuy:GoodGuy, badGuy:BadGuy }
+
+type alias BadGuy =
+  { x:Float, y:Float, vx:Float, vy:Float }
+
+type alias Player =
+  { x:Float, y:Float, vx:Float, vy:Float, lives:Int }
+
+type alias Game =
+  { state:State, badGuy:BadGuy, player1:Player }
+
 
 player : Float -> Player
-player x y = { x=x, y=y, vx=0, vy=0 }
+player x =
+  { x = x, y = 0, vx = 0, vy = 0, lives = 3 }
 
 defaultGame : Game
 defaultGame =
-    { state = Pause
-    , goodGuy = player -300 -150
-    , badGuy = player 0 0
-     }
+  { state   = Pause
+  , badGuy  = { x=0, y=0, vx=300, vy=300 }
+  , player1 = player (15-halfWidth)
+  }
 
--- Update the game
 
-near : Float -> Float -> Float -> Bool
-near n c m = m >= n-c && m <= n+c
+type alias Input =
+    { space : Bool
+    , dir1 : Int
+    , dir2 : Int
+    , delta : Time
+    }
 
-within : Player -> Player -> Bool
-within goodGuy badGuy =
-    (goodGuy.x |> near badGuy.x 8) && (goodGuy.y |> near badGuy.y 20)
 
--- change the direction of a velocity based on collisions
-stepV : Float -> Bool -> Float
+-- UPDATE
+
+update : Input -> Game -> Game
+update {space,dir1,dir2,delta} ({state,badGuy,player1} as game) =
+  let newState =
+        if  | space            -> Play
+            | otherwise        -> state
+
+      newBadGuy =
+        if state == Pause
+            then badGuy
+            else updateBadGuy delta badGuy
+
+  in
+      { game |
+          state   <- newState,
+          badGuy  <- newBadGuy,
+          player1 <- updatePlayer delta dir1 dir2 player1
+      }
+
+
+updateBadGuy : Time -> BadGuy -> BadGuy
+updateBadGuy t ({x,y,vx,vy} as badGuy) =
+  physicsUpdate t
+    { badGuy |
+        vx <- stepV vx (x < 7-halfWidth) (x > halfWidth-7),
+        vy <- stepV vy (y < 7-halfHeight) (y > halfHeight-7)
+    }
+
+
+updatePlayer : Time -> Int -> Int -> Player -> Player
+updatePlayer t dir1 dir2 player =
+  let player1 = physicsUpdate  t { player | vx <- toFloat dir1 * 200,
+                                            vy <- toFloat dir2 * 200}
+  in
+      { player1 |
+          y <- clamp (15-halfHeight) (halfHeight-15) player1.y,
+          x <- clamp (15-halfWidth) (halfWidth-15) player1.x
+      }
+
+
+physicsUpdate t ({x,y,vx,vy} as obj) =
+  { obj |
+      x <- x + vx * t,
+      y <- y + vy * t
+  }
+
+
+near k c n =
+    n >= k-c && n <= k+c
+
+within badGuy player1 =
+    near player1.x 8 badGuy.x && near player1.y 20 badGuy.y
+
+
 stepV v lowerCollision upperCollision =
-  if | lowerCollision  -> abs v
+  if | lowerCollision -> abs v
      | upperCollision -> 0 - abs v
-     | otherwise       -> v
-
--- step the position of an object based on its velocity and a timestep
-stepObj : Time -> Object a -> Object a
-stepObj t ({x,y,vx,vy} as obj) =
-    { obj | x <- x + vx * t
-          , y <- y + vy * t }
-
-{--
--- move a bad guy forward, detecting collisions with either paddle
-stepBadGuy : Time -> BadGuy -> Player -> BadGuy
-stepBadGuy t ({x,y,vx,vy} as badGuy) goodGuy =
-  if not (badGuy.x |> near 0 halfWidth)
-  then { badGuy | x <- 0, y <- 0 }
-  else
-    let vx' = stepV vx (badGuy `within` goodGuy)
-        vy' = stepV vy (y < 7-halfHeight)
-    in
-        stepObj t { badGuy | vx <- vx', vy <- vy' }
-
---}
-
--- step a player forward, making sure it does not fly off the court
-stepPlyr : Time -> Int -> Int -> Player -> Player
-stepPlyr t dir points player =
-  let player' = stepObj t { player | vy <- toFloat dir * 200 }
-      y'      = clamp (22-halfHeight) (halfHeight-22) player'.y
-      score'  = player.score + points
-  in
-      { player' | y <- y', score <- score' }
-
-stepGame : Input -> Game -> Game
-stepGame {space,player,delta}
-         ({state,badGuy,goodGuy} as game) =
-  let score1 = if badGuy.x >  halfWidth then 1 else 0
-      score2 = if ball.x < -halfWidth then 1 else 0
-
-      state' = state
-
-      badGuy' = if state == Pause then badGuy else
-                  stepBadGuy delta badGuy goodGuy
-
-      goodGuy' = stepPlyr delta paddle1 score1 player1
-  in
-      { game | state   <- state'
-             , badGuy  <- badGuy'
-             , goodGuy <- goodGuy' }
+     | otherwise      -> v
 
 
-gameState : Signal Game
-gameState = foldp stepGame defaultGame input
+-- VIEW
 
--- helper values
-pongGreen = rgb 60 100 60
-textGreen = rgb 160 200 160
-txt f = leftAligned << f << monospace << Text.color textGreen << fromString
-msg = "SPACE to start, WS and &uarr;&darr; to move"
-
--- shared function for rendering objects
-displayObj : Object a -> Shape -> Form
-displayObj obj shape =
-    move (obj.x,obj.y) (filled white shape)
-
--- display a game state
-display : (Int,Int) -> Game -> Element
-display (w,h) {state,badGuy,goodGuy} =
-  let scores : Element
-      scores = txt (Text.height 50) <|
-               toString goodGuy.score
+view : (Int,Int) -> Game -> Element
+view (w,h) {state,badGuy,player1} =
+  let lives : Element
+      lives = txt(Text.height 50) (toString player1.lives)
   in
       container w h middle <|
       collage gameWidth gameHeight
-       [ filled pongGreen   (rect gameWidth gameHeight)
-       , displayObj badGuy    (oval 15 15)
-       , displayObj goodGuy (rect 10 40)
-       , toForm scores
-           |> move (0, gameHeight/2 - 40)
-       , toForm (if state == Play then spacer 1 1 else txt id msg)
-           |> move (0, 40 - gameHeight/2)
-       ]
+        [ rect gameWidth gameHeight
+            |> filled pongGreen
+        , ngon 3 15
+            |> make badGuy
+        , ngon 5 15
+            |> make player1
+        , toForm lives
+            |> move (0, gameHeight/2 - 40)
+        , toForm (if state == Play then spacer 1 1 else txt identity msg)
+            |> move (0, 40 - gameHeight/2)
+        ]
 
-main = Signal.map2 display Window.dimensions gameState
+pongGreen = rgb 60 100 60
+textGreen = rgb 160 200 160
+txt f = Text.fromString >> Text.color textGreen >> Text.monospace >> f >> Text.leftAligned
+msg = "SPACE to start, &uarr;&darr;&larr;&rarr; to move"
+make obj shape =
+    shape
+      |> filled white
+      |> move (obj.x,obj.y)
